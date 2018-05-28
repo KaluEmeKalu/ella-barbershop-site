61c89d7067873f7fc3ccf3b23daa9e034ebc310f
Grace <1525436188@qq.com>
2018-05-28 10:04:48 +0800

    first commit

---
+++ haircut/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Appointment
+# Register your models here.
+class AppointmentAdmin(admin.ModelAdmin):
+	list_display = ['time', 'customer_name', 'barber_name']
+admin.site.register(Appointment, AppointmentAdmin)
---
+++ haircut/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from .models import Appointment
+def home_page(request):
+    if request.method == 'POST':
+        time = request.POST.get('time')
+        customer_name = request.POST.get("customer_name")
+        barber_name = request.POST.get('barber_name')
+        
+        appointment_obj = Appointment.objects.create(
+            time = time,
+            customer_name = customer_name,
+            barber_name = barber_name 
+        )
+        all_appointments = Appointment.objects.all()
+        context = {'appointments': all_appointments}
+        return render(request, 'haircut/home_page.html', context)
+    if request.method == "GET":
+        all_appointments = Appointment.objects.all()
+        context = {'appointments': all_appointments}
+        return render(request, 'haircut/home_page.html', context)
